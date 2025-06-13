d39b0f00a0c33ce09d42579b3d81fe3373abfaae
XuemeiCai <stephcaity@gmail.com>
2025-06-13 15:39:08 +0200

    fix url

--- BookAppApi/Program.cs
+++ BookAppApi/Program.cs
@@ -68,7 +68,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.Urls.Add($"https://*:{Environment.GetEnvironmentVariable("PORT") ?? "5001"}");
+app.Urls.Add($"http://*:{Environment.GetEnvironmentVariable("PORT") ?? "5000"}");
 
 app.UseAuthentication();  
 app.UseAuthorization();
